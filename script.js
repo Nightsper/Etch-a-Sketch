55229ff8a87ad8f519fc3a4843b63c084de0c961
Fahim Hossain <fahimhossain645@gmail.com>
2025-05-27 13:56:11 +0000

    Arrange div elements into a grid

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-
 let container = document.querySelector("#container")
 
-for (let i = 1; i < 16; i++) {
+for (let i = 0; i < 256; i++) {
   let box = document.createElement("div");
   
   box.style = "border: 1px solid black; width: 100px; height: 100px";
