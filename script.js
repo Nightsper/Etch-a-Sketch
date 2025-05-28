05b36d10167330691827903b9dcefa3ea670f0fe
Fahim Hossain <fahimhossain645@gmail.com>
2025-05-28 21:04:04 +0000

    Create function to dynamically change the grid layout

--- script.js
+++ script.js
@@ -1,11 +1,46 @@
 let container = document.querySelector("#container")
 
+let changeSize = document.querySelector("#changeSize")
 
-let grid = 16 * 16
+let defaultSize = 17
 
-for (let i = 0; i < grid; i++) {
-  let box = document.createElement("div");
-  container.appendChild(box)
-  box.setAttribute("class", "boxes")
-  box.addEventListener("mouseenter", () => {box.style.backgroundColor = "orange"})
+let contSize = 600;
+
+function createGrid(input) {
+  
+  
+  let gridCell = contSize / input
+  
+  for (let i = 0; i < input * input; i++) {
+    
+    let cells = document.createElement("div");
+    
+    cells.style.width = `${gridCell}px`
+    cells.style.height = `${gridCell}px`
+    
+    container.appendChild(cells)
+    
+    cells.setAttribute("class", "boxes")
+    cells.addEventListener("mouseenter", () => { cells.style.backgroundColor = "orange" })
+  }
+
+  changeSize.addEventListener("click", () => {
+    let input = prompt("Enter a number from 1 to 100. For ex. 17 equals to a 17 x 17 grid.")
+    if (input > 0 && input <= 100) {
+    removeGrid()
+    createGrid(input)
+    }
+    else if (input == null || input == "") {
+      return null
+    }
+    else (alert("The number you entered is not within 1 and 100!"))
+  })
+  
+  
 }
+
+function removeGrid() {
+  box.forEach((div) => div.remove())
+}
+
+createGrid(defaultSize)
