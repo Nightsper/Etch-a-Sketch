7f43f4e587daf46c900526aa15936fa6b2d8908b
Fahim Hossain <fahimhossain645@gmail.com>
2025-05-28 10:56:56 +0000

    Create a functional 16 x 16 grid

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 let container = document.querySelector("#container")
 
-let grid = 16 * 16;
+
+let grid = 16 * 16
 
 for (let i = 0; i < grid; i++) {
   let box = document.createElement("div");
-  
   container.appendChild(box)
   box.setAttribute("class", "boxes")
+  box.addEventListener("mouseenter", () => {box.style.backgroundColor = "orange"})
 }
