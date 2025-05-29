3730f079b4fc0bcfff2ded5bb95291c61d67c185
Fahim Hossain <fahimhossain645@gmail.com>
2025-05-29 14:44:07 +0000

    Change color from orange to random color

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 let container = document.querySelector("#container")
 let changeSizeBtn = document.querySelector("#changeSize")
+let blackBtn = document.querySelector("#black")
+let rainbowBtn = document.querySelector("#rainbow")
+let customBtn = document.querySelector("#custom")
+let inputColorPickerBtn = document.querySelector("#colorPicker")
+
 
 let defaultSize = 17
 let contSize = 600;
@@ -18,10 +23,13 @@ function createGrid(input) {
     
     cells.setAttribute("class", "boxes")
 
-cells.addEventListener("mouseenter", () => { cells.style.backgroundColor = "orange" })
+cells.addEventListener("mouseover" && "mousedown", () => { 
+  let colorValue = getRandomColor()
+  cells.style.backgroundColor = `${colorValue}`})
     
     container.appendChild(cells)
   }
+  
   changeSizeBtn.addEventListener("click", () => {
     let input = prompt("Enter a number from 1 to 100. For ex. 17 equals to a 17 x 17 grid.")
     
@@ -37,9 +45,34 @@ cells.addEventListener("mouseenter", () => { cells.style.backgroundColor = "oran
   
 }
 
-
 function removeGrid() {
   container.textContent= ""
 }
+
+function getRandomColor () {
+  let r = (Math.floor(Math.random() * 256))
+  let g = (Math.floor(Math.random() * 256))
+  let b = (Math.floor(Math.random() * 256))
+  return `rgb(${r}, ${g}, ${b})`
+}
+
+
+blackBtn.addEventListener("click", () => {
+  alert("Sorry! This button isn't functional yet.")
+}
+)
+
+rainbowBtn.addEventListener("click", () => {
+  alert("Sorry! This button isn't functional yet.")
+}
+)
+
+customBtn.addEventListener("click", () => {
+  alert("Sorry! This button isn't functional yet.")
+}
+)
+
+inputColorPickerBtn.disabled = true;
+
 
 createGrid(defaultSize)
