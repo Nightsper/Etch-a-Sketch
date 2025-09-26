6b1efd23a5fc15300904492118e246e7a3c0b1ce
Nightsper <fahimhossain645@gmail.com>
2025-09-26 21:32:23 +0600

    Fix bug for rainbow button

--- script.js
+++ script.js
@@ -87,7 +87,7 @@ function getRandomColor() {
   let r = (Math.floor(Math.random() * 256))
   let g = (Math.floor(Math.random() * 256))
   let b = (Math.floor(Math.random() * 256))
-  return rgb(`{r}, ${g}, ${b}`)
+  return rgb(`${r}, ${g}, ${b}`)
 }
 
 createGrid(defaultSize)
